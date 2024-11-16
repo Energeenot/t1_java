748e5c761abf2cc779f3a3f8a066b42ed25ab28a
Energeenot <abramov.pandos@yandex.ru>
2024-11-17 00:54:48 +0300

    добавил новые поля и связь с сущностью account

--- src/main/java/ru/t1/java/demo/model/Transaction.java
+++ src/main/java/ru/t1/java/demo/model/Transaction.java
@@ -1,11 +1,10 @@
 package ru.t1.java.demo.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.AbstractPersistable;
+import ru.t1.java.demo.model.enums.TransactionStatus;
 
 import java.time.LocalDateTime;
 
@@ -20,9 +19,20 @@ public class Transaction extends AbstractPersistable<Long> {
 
     @Column(name = "transaction_amount")
     private double transactionAmount;
-
     @CreationTimestamp
     @Column(name = "transaction_time", nullable = false, updatable = false)
     private LocalDateTime transactionTime;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private TransactionStatus status;
+    @Column(name = "transaction_id")
+    private String transactionId;
+    // зачем второй timestamp?  в первом дз ещё надо было добавить время транзакции
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
 
 }
