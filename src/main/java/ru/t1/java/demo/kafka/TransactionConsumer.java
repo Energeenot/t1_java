c135da2f2a5b7f2de780126ddfef3fe66232c806
Energeenot <abramov.pandos@yandex.ru>
2024-11-17 00:56:23 +0300

    изменил способ обработки топика t1_demo_transactions и добавил обработку топика t1_demo_transaction_result

--- src/main/java/ru/t1/java/demo/kafka/TransactionConsumer.java
+++ src/main/java/ru/t1/java/demo/kafka/TransactionConsumer.java
@@ -7,7 +7,9 @@ import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import ru.t1.java.demo.dto.TransactionDTO;
+import ru.t1.java.demo.dto.TransactionResultMessageDTO;
 import ru.t1.java.demo.model.Transaction;
+import ru.t1.java.demo.service.AccountService;
 import ru.t1.java.demo.service.TransactionService;
 import ru.t1.java.demo.util.TransactionMapper;
 
@@ -19,19 +21,32 @@ import java.util.List;
 public class TransactionConsumer {
 
     private final TransactionService transactionService;
+    private final AccountService accountService;
 
     @KafkaListener(topics = "t1_demo_transactions", containerFactory = "transactionKafkaListenerContainerFactory", groupId = "transaction")
     public void listen(@Payload List<TransactionDTO> transactionsDTO, Acknowledgment ack) {
         log.info("Received transactions list");
 
         try {
+            log.info("go in try");
             List<Transaction> transactions = transactionsDTO.stream()
-                    .map(TransactionMapper::toEntity)
+                    .map(dto -> TransactionMapper.toEntity(dto, accountService))
                     .toList();
+            log.info("Received {} transactions", transactions.size());
             transactionService.createTransactions(transactions);
         }finally {
             ack.acknowledge();
         }
         log.info("Finished listening transactions");
+    }
+
+    @KafkaListener(topics = "t1_demo_transaction_result", containerFactory = "transactionResultKafkaListenerContainerFactory", groupId = "transaction-result")
+    public void listenResult(@Payload TransactionResultMessageDTO resultMessageDTO, Acknowledgment ack){
+        log.info("Received transaction result");
+        try {
+            transactionService.processingResult(resultMessageDTO);
+        }finally {
+            ack.acknowledge();
+        }
     }
 }
