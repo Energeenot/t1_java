e1babfad89cf1b7e5daf6f9d1ff730aa233759d1
Energeenot <abramov.pandos@yandex.ru>
2024-11-17 00:58:42 +0300

    добавил метод поиска по уникальному id

--- src/main/java/ru/t1/java/demo/repository/TransactionRepository.java
+++ src/main/java/ru/t1/java/demo/repository/TransactionRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.t1.java.demo.model.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Transaction findByTransactionId(String transactionId);
 }
