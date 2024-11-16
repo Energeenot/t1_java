48ee90adb87e99e4fc1c3dea5b8ab249cb240e0f
Energeenot <abramov.pandos@yandex.ru>
2024-11-17 00:40:09 +0300

    добавил новые поля и связал с сущностью client

--- src/main/java/ru/t1/java/demo/model/Account.java
+++ src/main/java/ru/t1/java/demo/model/Account.java
@@ -3,6 +3,10 @@ package ru.t1.java.demo.model;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.AbstractPersistable;
+import ru.t1.java.demo.model.enums.AccountStatus;
+import ru.t1.java.demo.model.enums.AccountType;
+
+import java.util.List;
 
 @Entity
 @Table(name = "account")
@@ -16,11 +20,20 @@ public class Account extends AbstractPersistable<Long> {
     @Enumerated(EnumType.STRING)
     @Column(name = "account_type")
     private AccountType accountType;
-
     @Column(name = "balance")
     private double balance;
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private AccountStatus status;
+    @Column(name = "account_id")
+    private String accountId;
+    @Column(name = "frozen_amount")
+    private double frozenAmount;
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Transaction> transactions;
 
-    public enum AccountType{
-        DEBIT, CREDIT
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
 }
