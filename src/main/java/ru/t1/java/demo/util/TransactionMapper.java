cc498d0e29d75eb121b2997bf985bbbbf19fa5e3
Energeenot <abramov.pandos@yandex.ru>
2024-11-17 00:58:12 +0300

    изменил способ обработки транзакций

--- src/main/java/ru/t1/java/demo/util/TransactionMapper.java
+++ src/main/java/ru/t1/java/demo/util/TransactionMapper.java
@@ -1,14 +1,21 @@
 package ru.t1.java.demo.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import ru.t1.java.demo.dto.TransactionDTO;
+import ru.t1.java.demo.model.Account;
 import ru.t1.java.demo.model.Transaction;
+import ru.t1.java.demo.service.AccountService;
 
+@Component
+@Slf4j
 public class TransactionMapper {
 
-    public static Transaction toEntity(TransactionDTO dto) {
+    public static Transaction toEntity(TransactionDTO dto, AccountService accountService) {
+        Account account = accountService.getAccountById(dto.getAccountId());
         return Transaction.builder()
                 .transactionAmount(dto.getTransactionAmount())
-                .transactionTime(dto.getTransactionTime())
+                .account(account)
                 .build();
     }
 }
