0d1ab8bc2e5d4a550e3b24180a15b67d9f93d8f4
Energeenot <abramov.pandos@yandex.ru>
2024-11-24 01:52:34 +0300

    Добавил метод для поиска списка транзакций по статусу и id клиента

--- src/main/java/ru/t1/java/demo/repository/TransactionRepository.java
+++ src/main/java/ru/t1/java/demo/repository/TransactionRepository.java
@@ -2,7 +2,12 @@ package ru.t1.java.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.t1.java.demo.model.Transaction;
+import ru.t1.java.demo.model.enums.TransactionStatus;
+
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     Transaction findByTransactionId(String transactionId);
+
+    List<Transaction> findByStatusAndAccountId(TransactionStatus status, Long accountId);
 }
