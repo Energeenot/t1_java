1248d4442fd6604ff00876f0ee3fac829da8714d
Energeenot <abramov.pandos@yandex.ru>
2024-11-24 01:53:15 +0300

    Изменил логику сохранения транзакции

--- src/main/java/ru/t1/java/demo/service/TransactionService.java
+++ src/main/java/ru/t1/java/demo/service/TransactionService.java
@@ -1,26 +1,32 @@
 package ru.t1.java.demo.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import ru.t1.java.demo.aop.LogDataSourceError;
+import ru.t1.java.demo.dto.TransactionDTO;
 import ru.t1.java.demo.dto.TransactionRequestedDTO;
 import ru.t1.java.demo.dto.TransactionResultMessageDTO;
 import ru.t1.java.demo.model.Account;
 import ru.t1.java.demo.model.Client;
 import ru.t1.java.demo.model.Transaction;
 import ru.t1.java.demo.model.enums.AccountStatus;
+import ru.t1.java.demo.model.enums.ClientStatus;
 import ru.t1.java.demo.model.enums.TransactionStatus;
 import ru.t1.java.demo.repository.AccountRepository;
 import ru.t1.java.demo.repository.ClientRepository;
 import ru.t1.java.demo.repository.TransactionRepository;
+import ru.t1.java.demo.util.TransactionMapper;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -31,19 +37,73 @@ public class TransactionService {
     private final AccountRepository accountRepository;
     private final KafkaTemplate<String, TransactionRequestedDTO> kafkaTemplateTransaction;
     private final ClientRepository clientRepository;
+    private final ClientStatusService clientStatusService;
+    private final AccountService accountService;
+
+    @Value("${transaction.max-count}")
+    private int maxCount;
 
     @Autowired
-    public TransactionService(TransactionRepository transactionRepository, AccountRepository accountRepository, KafkaTemplate<String, TransactionRequestedDTO> kafkaTemplate, ClientRepository clientRepository) {
+    public TransactionService(TransactionRepository transactionRepository, AccountRepository accountRepository, KafkaTemplate<String, TransactionRequestedDTO> kafkaTemplate, ClientRepository clientRepository, ClientStatusService clientStatusService, AccountService accountService) {
         this.transactionRepository = transactionRepository;
         this.accountRepository = accountRepository;
         this.kafkaTemplateTransaction = kafkaTemplate;
         this.clientRepository = clientRepository;
+        this.clientStatusService = clientStatusService;
+        this.accountService = accountService;
     }
 
     @LogDataSourceError
-    public Transaction createTransaction(Transaction transaction) {
-        log.info("Create transaction");
+    public Transaction createTransaction(TransactionDTO transactionDTO) {
+        Transaction transaction = TransactionMapper.toEntity(transactionDTO, accountService);
+        transaction.setTransactionId(UUID.randomUUID().toString());
+        Account account = accountRepository.findById(transaction.getAccount().getId())
+                .orElseThrow(() -> new EntityNotFoundException("Account not found"));
+        Client client = clientRepository.findById(account.getClient().getId())
+                .orElseThrow(() -> new EntityNotFoundException("Client not found"));
+
+        if (client.getStatus() == null || client.getStatus() == ClientStatus.UNKNOWN){
+            handleUnknownClientStatus(client, account, transaction);
+        }
+        if (client.getStatus() == ClientStatus.BLOCKED){
+            log.info("Client is BLOCKED, rejecting transaction");
+            transaction.setStatus(TransactionStatus.REJECTED);
+        }
+        checkRejectedTransactionsThreshold(account, client, transaction);
         return transactionRepository.save(transaction);
+    }
+
+    private void checkRejectedTransactionsThreshold(Account account, Client client, Transaction transaction) {
+        List<Transaction> rejectedTransactions = transactionRepository.findByStatusAndAccountId(TransactionStatus.REJECTED, account.getId());
+        log.info("Found {} rejected transactions for account {}", rejectedTransactions.size(), account.getId());
+
+        if (rejectedTransactions.size() > maxCount && client.getStatus() != ClientStatus.UNKNOWN){
+            log.info("max count is reached and account will be arrested");
+            account.setStatus(AccountStatus.ARRESTED);
+            accountRepository.save(account);
+            transaction.setStatus(TransactionStatus.REJECTED);
+        }
+    }
+
+    private void handleUnknownClientStatus(Client client, Account account, Transaction transaction) {
+        log.info("Client {} status is unknown", client);
+        Map<String, String> response = clientStatusService.sendRequestToService2(client.getId(), account.getId());
+        log.info("Client {} request is {}", client, response);
+        String statusValue = response.get("status");
+        ClientStatus status = ClientStatus.valueOf(statusValue);
+        System.out.println(status + " status is");
+        if (status == ClientStatus.BLOCKED){
+            blockClientAndAccount(client, account);
+            transaction.setStatus(TransactionStatus.REJECTED);
+        }
+    }
+
+    private void blockClientAndAccount(Client client, Account account) {
+        log.info("Blocking client {} and account {}", client.getId(), account.getId());
+        client.setStatus(ClientStatus.BLOCKED);
+        account.setStatus(AccountStatus.BLOCKED);
+        clientRepository.save(client);
+        accountRepository.save(account);
     }
 
     @LogDataSourceError
@@ -77,9 +137,7 @@ public class TransactionService {
     public void createTransactions(List<Transaction> transactions) {
         transactions.forEach(transaction -> {
             log.info("Processing transaction {}", transaction);
-//            Hibernate.initialize(transaction.getAccount());
             Account account = accountRepository.findById(transaction.getAccount().getId()).get();
-//            Hibernate.initialize(account.getClient());
             Client client = clientRepository.findById(account.getClient().getId()).get();
             System.out.println(account + " this account has been processed");
             log.error("Account {}", account);
@@ -134,31 +192,4 @@ public class TransactionService {
             }
         }
     }
-
-//    protected void processingTransaction(Transaction transaction) {
-//        log.info("Processing transaction {}", transaction);
-//        Hibernate.initialize(transaction.getAccount());
-//        Account account = transaction.getAccount();
-//        Hibernate.initialize(account.getClient());
-//        System.out.println(account + " this account has been processed");
-//        log.error("Account {}", account);
-//        log.error("status {}", account.getStatus());
-//        if (account.getStatus() == AccountStatus.OPEN) {
-//            log.info("Account {} is open", account);
-//            transaction.setStatus(TransactionStatus.REQUESTED);
-//            transaction.setTransactionId(UUID.randomUUID().toString());
-//            transactionRepository.save(transaction);
-//            account.setBalance(account.getBalance() + transaction.getTransactionAmount());
-//            accountRepository.save(account);
-//            TransactionRequestedDTO requestedDTO = TransactionRequestedDTO.builder()
-//                    .clientId(account.getClient().getClientId())
-//                    .accountId(account.getAccountId())
-//                    .transactionId(transaction.getTransactionId())
-//                    .timestamp(transaction.getTimestamp())
-//                    .transactionAmount(transaction.getTransactionAmount())
-//                    .accountBalance(account.getBalance())
-//                    .build();
-//            kafkaTemplateTransaction.send("t1_demo_transaction_accept", requestedDTO);
-//        }
-//    }
 }
