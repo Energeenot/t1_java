983154b3bccf5a4984f5a15524762d0e737aeb1d
Energeenot <abramov.pandos@yandex.ru>
2024-12-03 18:58:25 +0300

    теперь аннотация из стартера

--- src/main/java/ru/t1/java/demo/service/AccountService.java
+++ src/main/java/ru/t1/java/demo/service/AccountService.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-import ru.t1.java.demo.aop.LogDataSourceError;
+import ru.Energeenot.logging_aspect_starter.annotations.LogDataSourceError;
 import ru.t1.java.demo.model.Account;
 import ru.t1.java.demo.repository.AccountRepository;
 
