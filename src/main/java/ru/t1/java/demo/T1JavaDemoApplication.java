241107ce02db48fccaa164573a98a9732c48fb55
Energeenot <abramov.pandos@yandex.ru>
2024-12-14 01:47:32 +0300

    Добавлена аннотация для отложенных задач

--- src/main/java/ru/t1/java/demo/T1JavaDemoApplication.java
+++ src/main/java/ru/t1/java/demo/T1JavaDemoApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @Slf4j
 @EnableJpaRepositories(basePackages = "ru.t1.java.demo.repository")
 @EntityScan(basePackages = "ru.t1.java.demo.model")
+@EnableScheduling
 public class T1JavaDemoApplication {
 
     public static void main(String[] args) {
