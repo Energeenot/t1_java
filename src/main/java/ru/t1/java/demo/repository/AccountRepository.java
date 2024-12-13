f21452dba0cafa9f4a2c3e4ff8e155174c4fdfc6
Energeenot <abramov.pandos@yandex.ru>
2024-12-14 01:45:33 +0300

    Добавлены методы для поиска арестованных счетов с ограничением и без

--- src/main/java/ru/t1/java/demo/repository/AccountRepository.java
+++ src/main/java/ru/t1/java/demo/repository/AccountRepository.java
@@ -1,8 +1,18 @@
 package ru.t1.java.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.t1.java.demo.model.Account;
+
+import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByAccountId(String accountId);
+
+    @Query(value = "SELECT * FROM account WHERE status = :status LIMIT :limit", nativeQuery = true)
+    List<Account> findArrestedAccountsWithLimit(@Param("status") String status,@Param("limit") int numberOfUnblockingAccounts);
+
+    @Query("SELECT COUNT(a) FROM Account a WHERE a.status = 'ARRESTED'")
+    long countArrestedAccounts();
 }
