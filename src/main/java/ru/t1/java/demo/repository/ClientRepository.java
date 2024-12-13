7aabc05ce36ee7cf8c856a29b22fac0c229c693c
Energeenot <abramov.pandos@yandex.ru>
2024-12-14 01:46:00 +0300

    Добавлены методы для поиска заблокированных клиентов с ограничением и без

--- src/main/java/ru/t1/java/demo/repository/ClientRepository.java
+++ src/main/java/ru/t1/java/demo/repository/ClientRepository.java
@@ -1,11 +1,21 @@
 package ru.t1.java.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.t1.java.demo.model.Client;
+import ru.t1.java.demo.model.enums.ClientStatus;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
     @Override
     Optional<Client> findById(Long aLong);
+
+    @Query(value = "SELECT * FROM client WHERE status = :status LIMIT :limit", nativeQuery = true)
+    List<Client> findBlockedClientsWithLimit(@Param("status") String status, @Param("limit") int limit);
+
+    @Query("SELECT COUNT(c) FROM Client c WHERE c.status = 'BLOCKED'")
+    long countBlockedClients();
 }
